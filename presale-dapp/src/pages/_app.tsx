bfee5f0b122da0e4acbed4d54a8ee153c67926e6
Nazlul Rizan <eng.nazrizan@gmail.com>
2024-10-14 17:02:21 +0530

    ui changes

--- presale-dapp/src/pages/_app.tsx
+++ presale-dapp/src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import '../styles/globals.css';
-import '../styles/fontawesome.ts';
 import '@rainbow-me/rainbowkit/styles.css';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
